c292300996ec7cb3906a279e2dee2258701826ba
Nicola Stroffolino <nicola.stroffolino@studio.unibo.it>
2025-12-05 13:23:44 +0100

    Reactive GUI 2

--- 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -1,7 +1,18 @@
 package it.unibo.oop.reactivegui02;
 
-import javax.swing.JFrame;
 import java.io.Serial;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import it.unibo.oop.JFrameUtil;
 
 /**
  * Second example of reactive GUI.
@@ -10,4 +21,88 @@ public final class ConcurrentGUI extends JFrame {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConcurrentGUI.class);
+
+    private final JLabel display = new JLabel();
+
+    public ConcurrentGUI() {
+        super();
+        JFrameUtil.dimensionJFrame(this);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        final JButton up = new JButton("up");
+        final JButton down = new JButton("down");
+        final JButton stop = new JButton("stop");
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+        /*
+         * Register a listener that stops it
+         */
+        stop.addActionListener(e -> agent.stopCounting());
+        up.addActionListener(e -> agent.changeDirection(Direction.UP));
+        down.addActionListener(e -> agent.changeDirection(Direction.DOWN));
+    }
+
+    private final class Agent implements Runnable {
+        /*
+         * Stop is volatile to ensure visibility. Look at:
+         *
+         * http://archive.is/9PU5N - Sections 17.3 and 17.4
+         *
+         * For more details on how to use volatile:
+         *
+         * http://archive.is/4lsKW
+         *
+         */
+        private volatile boolean stop;
+        private volatile Direction currentDirection = Direction.UP;
+        private int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    // The EDT doesn't access `counter` anymore, it doesn't need to be volatile
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+                    this.counter += currentDirection.getStep();
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    LOGGER.error(ex.getMessage(), ex);
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void changeDirection(final Direction newDirection) {
+            this.currentDirection = newDirection;
+        }
+    }
+
+    private enum Direction {
+        UP(1),
+        DOWN(-1);
+
+        private final int step;
+
+        Direction(int i) {
+            this.step = i;
+        }
+        
+        public int getStep() {
+            return step;
+        }
+    }
 }
